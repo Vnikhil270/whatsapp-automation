953ef67827d5825c5da5d3f8b3cd3854ac5b1b19
Nikhil <uditvermaksp@gmail.com>
2021-11-13 16:49:30 +0530

    initial commit

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import pyperclip
 import time
 
+# this imports the name_input.py file which takes user input
+import name_input
+
+msg = input("Enter the Message :")
 # 1.creating an obect to open chrome, 2.And paste the path of chrome driver
 browser = webdriver.Chrome(r'C:\Users\Dell\Downloads\chromedriver_win32\chromedriver.exe')
 browser.maximize_window()
@@ -15,23 +18,8 @@ browser.maximize_window()
 # passing the url
 browser.get('https://web.whatsapp.com/')
 
-with open("groups.txt", "r", encoding="utf8") as f:
-    groups =[group.strip() for group in f.readlines()]
-
-groups = []
- 
-# number of people/groups as input
-n = int(input("Enter number of group-name/person you want to send message : "))
- 
-# iterating till the range
-for i in range(0, n):
-    ele = input()
- 
-    groups.append(ele) # adding the element
-    
-msg = input("Enter the Message :")
-
-for group in groups:
+# calling groups list present in name_input.py file through reference
+for group in name_input.groups:
     
     # searching the search box present in whatsapp page
     search_xpath = '//div[@role="textbox"][@contenteditable="true"][@data-tab="3"]'
@@ -48,18 +36,19 @@ for group in groups:
     time.sleep(2)
 
     # getting and opening the name of the person or group 
-    name_xpath = f'//span[@title="{group}"]'     #f is used since it is a dynamic colonomal
+    name_xpath = f'//span[@dir="auto"][@title="{group}"]'     #f is used since it is a dynamic colonomal
     name_title = browser.find_element_by_xpath(name_xpath)
     name_title.click()
 
     time.sleep(2)
 
     # selecting the msg box for the writing process
-    input_xpath = '//div[@contenteditable="true"][@data-tab="1"]'
+    input_xpath = '//div[@contenteditable="true"][@data-tab="9"]'
     input_box = browser.find_element_by_xpath(input_xpath)
    
     pyperclip.copy(msg)
     # This function paste the text-message as it is
-    input_box.send_keys(Keys.CONTROL + Keys.v)
+    input_box.send_keys(Keys.CONTROL + "v")
     input_box.send_keys(Keys.ENTER)
     
+    time.sleep(15)
